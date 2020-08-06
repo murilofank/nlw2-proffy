ac0d7755b76e134aea65662ca513216a613e0134
Murilo Fank <murilo.fank@universo.univates.br>
2020-08-06 01:51:22 -0300

    commit inicio do back-end; criação do banco de dados

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json([])
+})
+
+
+app.listen(3333);
